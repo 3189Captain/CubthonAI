bfb7a1b5dde17e0c64ab376b44efc5adc01b8935
3189Captain <oogabooga300@gmail.com>
2020-01-12 21:46:05 -0700

    upload project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+	bool gameHasEnded = false;
+	public float restartDelay = 1f;
+	public GameObject completeLevelUI;
+	public void EndGame()
+	{
+		if (!gameHasEnded)
+		{
+			gameHasEnded = true;
+			Invoke("Restart", restartDelay);
+		}
+	}
+
+	public void CompleteLevel()
+	{
+		Debug.Log("Level 1");
+		completeLevelUI.SetActive(true);
+	}
+
+	void Restart()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+}
---
+++ Assets/Seek.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seek : MonoBehaviour
+{
+    public GameObject player; //reference to player
+    public Rigidbody rb; //reference to this object's rigidbody
+    public float speedMult = 0.25f; //multiplier for AI travel speed
+    Vector3 diffVector; //vector calculated by taking the difference between this object's postion and the player
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        diffVector = player.transform.position - transform.position; //calculate diffVector
+        transform.rotation = Quaternion.LookRotation(diffVector.normalized); //look towards player
+        rb.AddForce(diffVector.normalized * speedMult, ForceMode.VelocityChange); //move towards player
+    }
+}
